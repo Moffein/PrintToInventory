d2d93428f409a734f300d945a6d08cba12a79b54
Moffein <22742925+Moffein@users.noreply.github.com>
2025-11-22 22:32:17 -0600

    DLC3 fixes, MP-only config

--- PrintToInventory/Class1.cs
+++ PrintToInventory/Class1.cs
@@ -23,7 +23,7 @@ namespace R2API.Utils
 namespace PrintToInventory
 {
 	[BepInDependency("com.funkfrog_sipondo.sharesuite", BepInDependency.DependencyFlags.SoftDependency)]
-	[BepInPlugin("com.Moffein.PrintToInventory", "PrintToInventory", "1.2.1")]
+	[BepInPlugin("com.Moffein.PrintToInventory", "PrintToInventory", "1.3.0")]
 
     public class PrintToInventory : BaseUnityPlugin
     {
@@ -33,6 +33,8 @@ namespace PrintToInventory
 		public static bool affectCauldrons = true;
 		public static bool affectCleanse = true;
 
+		public static bool multiplayerOnly = true;
+
 		public void Awake()
         {
 			if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.funkfrog_sipondo.sharesuite"))
@@ -40,50 +42,111 @@ namespace PrintToInventory
 				Debug.LogError("PrintToInventory: ShareSuite detected! PrintToInventory will be disabled due to incompatibilities.");
 				return;
 			}
-
-			affectCauldrons = Config.Bind("General", "Cauldron", true, "Affect this interactable.").Value;
+			
+			multiplayerOnly = Config.Bind("General", "Multiplayer Only", true, "Only active this mod in multiplayer.").Value;
+            affectCauldrons = Config.Bind("General", "Cauldron", true, "Affect this interactable.").Value;
 			affectPrinters = Config.Bind("General", "Printer", true, "Affect this interactable.").Value;
 			affectScrappers = Config.Bind("General", "Scrapper", true, "Affect this interactable.").Value;
 			affectCleanse = Config.Bind("General", "Cleansing Pool", true, "Affect this interactable.").Value;
 
-			if (affectPrinters) On.EntityStates.Duplicator.Duplicating.OnEnter += Duplicating_OnEnter;
-
-			if (affectCauldrons || affectCleanse)
-            {
-				On.RoR2.PurchaseInteraction.OnInteractionBegin += ImmediatelyGiveItem;
-				On.RoR2.ShopTerminalBehavior.DropPickup += PreventDrop;
-			}
-			if (affectScrappers)
-            {
-				IL.EntityStates.Scrapper.ScrappingToIdle.OnEnter += (il) =>
-				{
-					ILCursor c = new ILCursor(il);
-					c.GotoNext(x => x.MatchLdloc(0), x => x.MatchLdsfld(typeof(PickupIndex), "none"));
-					c.Index++;
-					c.Emit(OpCodes.Ldarg_0);
-					c.EmitDelegate<Func<PickupIndex, ScrappingToIdle, PickupIndex>>((pickupIndex, self) =>
-					{
-						if (pickupIndex != PickupIndex.none && self.scrapperController && self.scrapperController.interactor)
-                        {
-							CharacterBody cb = self.scrapperController.interactor.GetComponent<CharacterBody>();
-							if (cb && cb.inventory)
-							{
-								PickupDef pd = PickupCatalog.GetPickupDef(pickupIndex);
-								if (pd != null && pd.itemIndex != ItemIndex.None)
-								{
-									cb.inventory.GiveItem(pd.itemIndex, self.scrapperController.itemsEaten);
-									self.scrapperController.itemsEaten = 0;
-									return PickupIndex.none;
-								}
-							}
-						}
-						return pickupIndex;
-					});
-				};
-            }
+            On.RoR2.Run.Start += Run_Start;
+            On.RoR2.Run.OnDestroy += Run_OnDestroy;
 		}
 
-		private void Duplicating_OnEnter(On.EntityStates.Duplicator.Duplicating.orig_OnEnter orig, EntityStates.Duplicator.Duplicating self)
+        private void Run_OnDestroy(On.RoR2.Run.orig_OnDestroy orig, Run self)
+        {
+			orig(self);
+			RemoveHooks();
+        }
+
+        private void Run_Start(On.RoR2.Run.orig_Start orig, Run self)
+        {
+			orig(self);
+			if (!multiplayerOnly || !RoR2Application.isInSinglePlayer)
+			{
+				AddHooks();
+			}
+        }
+
+        private static bool hooksAdded = false;
+		public void AddHooks()
+		{
+			if (hooksAdded) return;
+			hooksAdded = true;
+
+            if (affectPrinters) On.EntityStates.Duplicator.Duplicating.OnEnter += Duplicating_OnEnter;
+
+            if (affectCauldrons || affectCleanse)
+            {
+                On.RoR2.PurchaseInteraction.OnInteractionBegin += ImmediatelyGiveItem;
+                On.RoR2.ShopTerminalBehavior.DropPickup += PreventDrop;
+            }
+            if (affectScrappers)
+            {
+                IL.EntityStates.Scrapper.ScrappingToIdle.OnEnter += ScrappingToIdle_DirectToInventory;
+            }
+        }
+
+		public void RemoveHooks()
+		{
+			if (!hooksAdded) return;
+			hooksAdded = false;
+
+            if (affectPrinters) On.EntityStates.Duplicator.Duplicating.OnEnter -= Duplicating_OnEnter;
+
+            if (affectCauldrons || affectCleanse)
+            {
+                On.RoR2.PurchaseInteraction.OnInteractionBegin -= ImmediatelyGiveItem;
+                On.RoR2.ShopTerminalBehavior.DropPickup -= PreventDrop;
+            }
+            if (affectScrappers)
+            {
+                IL.EntityStates.Scrapper.ScrappingToIdle.OnEnter -= ScrappingToIdle_DirectToInventory;
+            }
+        }
+
+        private void ScrappingToIdle_DirectToInventory(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            if (c.TryGotoNext(MoveType.After, x => x.MatchCallvirt<ScrapperController>("IsRewardPickupQueueEmpty")))
+            {
+                c.Emit(OpCodes.Ldarg_0);
+				c.EmitDelegate<Func<bool, EntityStates.Scrapper.ScrappingToIdle, bool>>((queueEmpty, self) =>
+				{
+					if (!queueEmpty && self.scrapperController && self.scrapperController.interactor && self.scrapperController.pickupPrintQueue != null)
+					{
+						CharacterBody cb = self.scrapperController.interactor.GetComponent<CharacterBody>();
+						if (cb && cb.inventory)
+						{
+							List<UniquePickup> processedPickups = new List<UniquePickup>();
+							foreach (UniquePickup pickup in self.scrapperController.pickupPrintQueue)
+							{
+								PickupDef pd = PickupCatalog.GetPickupDef(pickup.pickupIndex);
+								if (pd != null && pd.itemIndex != ItemIndex.None)
+								{
+									cb.inventory.GiveItemPermanent(pd.itemIndex);
+									processedPickups.Add(pickup);
+								}
+							}
+
+							foreach (UniquePickup up in processedPickups)
+							{
+									self.scrapperController.pickupPrintQueue.Remove(up);
+							}
+
+							return self.scrapperController.IsRewardPickupQueueEmpty();
+						}
+					}
+					return queueEmpty;
+                });
+            }
+            else
+            {
+                Debug.LogError("PrintToInventory: Scrapper IL hook failed.");
+            }
+        }
+
+        private void Duplicating_OnEnter(On.EntityStates.Duplicator.Duplicating.orig_OnEnter orig, EntityStates.Duplicator.Duplicating self)
 		{
 			orig(self);
 			if (NetworkServer.active)
@@ -95,7 +158,7 @@ namespace PrintToInventory
 					CharacterBody cb = pi.lastActivator.GetComponent<CharacterBody>();
 					if (cb && cb.inventory)
 					{
-						PickupDef pd = PickupCatalog.GetPickupDef(stb.pickupIndex);
+						PickupDef pd = PickupCatalog.GetPickupDef(stb.pickup.pickupIndex);
 						if (pd != null && pd.itemIndex != ItemIndex.None)
 						{
 							cb.inventory.GiveItem(pd.itemIndex, 1);
@@ -126,7 +189,7 @@ namespace PrintToInventory
 
 					if (cb && cb.inventory && stb)
 					{
-						PickupDef pd = PickupCatalog.GetPickupDef(stb.pickupIndex);
+						PickupDef pd = PickupCatalog.GetPickupDef(stb.pickup.pickupIndex);
 						if (pd != null && pd.itemIndex != ItemIndex.None)
 						{
 							int dropCount = 1;
